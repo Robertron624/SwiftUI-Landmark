b728bbb57e15e871ebfad1e4cd59b655ad723575
Roberto Ramirez <robert.624@hotmail.com>
2023-03-05 12:23:43 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Robert Ramirez on 5/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
