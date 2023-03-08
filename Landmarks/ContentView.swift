4e03363bc2587553886455c7fb191f974bd30a6f
Roberto Ramirez <robert.624@hotmail.com>
2023-03-07 22:17:23 -0500

    added image, map and info text

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y:-130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.red)
+                    .multilineTextAlignment(.center)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer();
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Turtle Rock is a neighborhood in Irvine, California with a population of 10,114. Turtle Rock is in Orange County and is one of the best places to live in California. Living in Turtle Rock offers residents a dense suburban feel and most residents own their homes. In Turtle Rock there are a lot of restaurants, coffee shops, and parks.")
+            }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
